57ec5aa635e35d1ee84e6268b1f3aca156584d82
connorwarme <connor.warme@gmail.com>
2022-08-29 19:36:33 -0600

    Initial attempt at writing a promise

--- promise.js
+++ promise.js
@@ -1,1 +1,17 @@
 // practice deploying JS promises
+const add = (xPromise, yPromise) => {
+    console.log([xPromise, yPromise]);
+    // return value waits for both x and y to be provided before firing
+    return Promise.all([xPromise, yPromise])
+    .then((array) => array[0] + array[1]);
+}
+const fetchX = (value) => {
+    console.log(value);
+    return Promise.resolve(value);
+}
+const fetchY = (value) => {
+    console.log(value);
+    return Promise.resolve(value);
+}
+add(fetchX(1), fetchY(2))
+.then((sum) => console.log(sum));
