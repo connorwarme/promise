a2c275e5f5088230772490ce6d1bf922e704e47b
connorwarme <connor.warme@gmail.com>
2022-08-30 18:15:56 -0600

    Refresh img button & fn

--- promise.js
+++ promise.js
@@ -45,11 +45,35 @@ delay(100)
 
 // learn how to use APIs
 const main = document.querySelector('div.main');
+main.classList.add('main');
+const picContainer = document.createElement('div');
+picContainer.className = 'picContainer';
 const picture = document.createElement('img');
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=KItDMzaj55RdWIHhN5eJNf6eoNvcj3eE&s=cats', {
     mode: "cors"
 })
+  .then((response) => response.json())
   .then((response) => {
-    console.log(response.json());
-  });
-main.appendChild(picture);
+    console.log(response.data.images.original.url);
+    picture.src = response.data.images.original.url;
+  })
+main.appendChild(picContainer);
+picContainer.appendChild(picture);
+const refresh = document.createElement('button');
+refresh.textContent = "Refresh";
+main.appendChild(refresh);
+
+const kitties = () => {
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=KItDMzaj55RdWIHhN5eJNf6eoNvcj3eE&s=cats', {
+    mode: "cors"
+})
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.data.images.original.url);
+    picture.src = response.data.images.original.url;
+  })
+}
+refresh.addEventListener('click', () => {
+    console.log('fire');
+    kitties();
+});
